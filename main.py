e7960a8bda75be48c0ee38bf11c4e58d8b4ef9c3
BillStephens2022 <two4onebill@yahoo.com>
2023-04-05 11:52:23 -0400

    when user clicks on checkmark, it removes the word from words to learn

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")  #converts csv data to a dictionary
 current_card = {}
+to_learn = {}
+
+try:    
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:   
+    to_learn = data.to_dict(orient="records")  #converts csv data to a dictionary
+
 
 def next_card():
     global current_card, flip_timer
@@ -21,6 +28,15 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+#when user clicks on checkmark, the user is indicating that the word is known, so it is removed from this list of words to learn.
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv")
+    next_card()
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -41,7 +57,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, highlightbackgr
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
